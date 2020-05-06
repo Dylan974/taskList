16b4f41ac7c7f55f4550ed3703216a4662e7d66f
Dylan <mugidylan@gmail.com>
2020-05-06 22:53:13 +0400

    add tasks list

--- src/components/TasksEpic/TasksContainer.js
+++ src/components/TasksEpic/TasksContainer.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
+import TasksList from './TasksList';
 
 function TasksContainer(props) {
     const [tasks, setTasks] = useState([{ title: 'Nouvelle tâche', completed: false }]);
 
     return (
         <View>
-            <Text>{tasks[0].title}</Text>
+            <TasksList tasks={tasks} />
         </View>
     );
 }
