e11d6362f9dc82eb6de70de14cc2eeb46e6e91e9
Dylan <mugidylan@gmail.com>
2020-05-07 00:48:17 +0400

    redisplay taskTile

--- src/components/TasksEpic/TaskTile.js
+++ src/components/TasksEpic/TaskTile.js
@@ -8,15 +8,15 @@ const TaskTile = ({ id, title, completed, onChangeStatus, onDeleteTask }) => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.subcontainer}>
-                <TouchableHighlight onPress={() => onChangeStatus(id)}>
+            <TouchableHighlight onPress={() => onChangeStatus(id)}>
+                <View style={styles.subcontainer}>
                     <Image
                         style={styles.icon}
                         source={completed ? require('../../../assets/icon_check.png') : require('../../../assets/icon_circle.png')}
                     />
                     <Text style={[styles.title, { color: completed ? 'lightgrey' : 'black' }]}>{title}</Text>
-                </TouchableHighlight>
-            </View>
+                </View>
+            </TouchableHighlight>
             <Image
                 style={styles.icon}
                 source={require('../../../assets/icon_bin.png')}
