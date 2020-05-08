2345ced391feda1b006182995848fecc14563600
Dylan <mugidylan@gmail.com>
2020-05-08 18:44:12 +0400

    add floating button

--- src/components/TasksEpic/TasksContainer.js
+++ src/components/TasksEpic/TasksContainer.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import TasksList from './TasksList';
 import TaskForm from './TaskForm';
 import CountersContainer from './CountersContainer';
+import FloatingButton from '../_Shared/FloatingButton';
 
 function TasksContainer(props) {
     const [tasks, setTasks] = useState([{ id: new Date().getTime().toString(), title: 'Nouvelle tâche', completed: false }]);
+
+    const [isFormOpened, setIsFormOpened] = useState(false);
 
     const onAddTask = (title) => {
         setTasks([{ id: new Date().getTime().toString(), title, completed: false }, ...tasks]);
@@ -37,13 +40,25 @@ function TasksContainer(props) {
         return counter;
     };
 
+    const toggleForm = () => {
+        setIsFormOpened(!isFormOpened);
+    }
+
     return (
-        <View>
-            <TaskForm onAddTask={onAddTask} />
+        <View style={syles.container}>
+            {isFormOpened && <TaskForm onAddTask={onAddTask} />}
             <CountersContainer nbTasks={tasks.length} nbTasksCompleted={() => getTasksCompleted()} />
             <TasksList tasks={tasks} onChangeStatus={onChangeStatus} onDeleteTask={onDeleteTask} />
+            <FloatingButton toggleForm={toggleForm} isFormOpened={isFormOpened} />
         </View>
     );
 };
+
+const syles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: 'relative'
+    }
+});
 
 export default TasksContainer;
