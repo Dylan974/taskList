e705e8e88cabb32e354b69d8841dbf3c5cee4f05
Dylan <mugidylan@gmail.com>
2020-05-08 18:14:06 +0400

    add counters component

--- src/components/TasksEpic/CountersContainer.js
+++ src/components/TasksEpic/CountersContainer.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Counter from '../_Shared/Counter';
+
+const CountersContainer = ({ nbTasks, nbTasksCompleted }) => {
+    return (
+        <View style={styles.container}>
+            <Counter nb={nbTasks} title={'Tâches crées'}></Counter>
+            <Counter nb={nbTasksCompleted} title={'Tâches complétees'}></Counter>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 25,
+    }
+});
+
+export default CountersContainer;
--- src/components/TasksEpic/TasksContainer.js
+++ src/components/TasksEpic/TasksContainer.js
@@ -2,20 +2,19 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 import TasksList from './TasksList';
 import TaskForm from './TaskForm';
+import CountersContainer from './CountersContainer';
 
 function TasksContainer(props) {
     const [tasks, setTasks] = useState([{ id: new Date().getTime().toString(), title: 'Nouvelle tâche', completed: false }]);
 
     const onAddTask = (title) => {
         setTasks([{ id: new Date().getTime().toString(), title, completed: false }, ...tasks]);
-    }
+    };
 
     const onDeleteTask = (id) => {
-        console.log('on delete');
         const newTasks = tasks.filter(task => task.id != id);
-        console.log(newTasks);
         setTasks([...newTasks]);
-    }
+    };
 
     const onChangeStatus = (id) => {
         let newTasks = [];
@@ -27,14 +26,24 @@ function TasksContainer(props) {
             }
         })
         setTasks(newTasks);
-    }
+    };
+
+    const getTasksCompleted = () => {
+        let counter = 0;
+
+        tasks.forEach(task => {
+            if (task.completed) counter++;
+            return counter;
+        })
+    };
 
     return (
         <View>
             <TaskForm onAddTask={onAddTask} />
+            <CountersContainer nbTasks={tasks.length} nbTasksCompleted={() => getTasksCompleted()} />
             <TasksList tasks={tasks} onChangeStatus={onChangeStatus} onDeleteTask={onDeleteTask} />
         </View>
     );
-}
+};
 
 export default TasksContainer;
